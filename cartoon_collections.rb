6911a7e051e230e259221b4dff7b3403fae21cf9
calm-block-7153 <rebeccahunt034@gmail.com>
2019-12-15 11:29:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
 def roll_call_dwarves(array)
-  array.collect.each_with_index{|names|array.join(names"\n")}
+  array.collect.each_with_index do|names|
+  array.join(" \n")
+  puts array
 end
 
 def summon_captain_planet# code an argument here
