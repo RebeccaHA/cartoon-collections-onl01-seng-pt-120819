95172160a31c32e3752178b85fc2deb22324a4e2
calm-block-7153 <rebeccahunt034@gmail.com>
2019-12-15 12:16:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,6 @@ end
 def find_the_cheese(cheddar_cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
     cheddar_cheese.detect do |cheese|
-      cheese == "cheddar" 
-end
+      cheese == cheese_types
+    end
 end
