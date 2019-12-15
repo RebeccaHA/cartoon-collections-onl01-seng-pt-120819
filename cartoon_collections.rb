566d060f10949849fa021a1a80bc27ec14e41207
calm-block-7153 <rebeccahunt034@gmail.com>
2019-12-15 11:46:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 
 def summon_captain_planet(array)
   array.collect do |veggies|
-    puts "#{veggies.capitalize}"
+    puts "#{veggies.capitalize}!"
 end
 
 def long_planeteer_calls# code an argument here
