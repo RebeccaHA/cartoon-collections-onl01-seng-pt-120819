7486fa09c47bd0ee1b003d09242970f672497590
calm-block-7153 <rebeccahunt034@gmail.com>
2019-12-15 11:51:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.collect.capitalize do |veggies|
-    puts "#{veggies}!"
+  array.collect do |veggies|
+    puts "#{veggies.capitalize}!"
   end
   array
   binding.pry
